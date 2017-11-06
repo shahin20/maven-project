536525948233cdc5968efc738afaffea221dd906
Islam <mdislam@deloitte.com>
2017-11-06 16:55:32 -0500

    Added new header property content type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,7 +1,11 @@
 package com.mulesoft.training;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+
+import static org.junit.Assert.*;
+
 import org.junit.Rule;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -19,6 +23,14 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "mule-maven-training.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\n\n------------Dynamic port in test case 2 ___________________>"+myPort.getNumber());
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 
 }
